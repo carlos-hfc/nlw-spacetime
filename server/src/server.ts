0ccc731896f4eef2afdb2aa150c6d35e3b880284
Carlos Faustino <chfcchfc96@gmail.com>
2024-05-03 16:25:01 -0300

    feat: add auth routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
+import "dotenv/config"
+
 import fastifyCors from "@fastify/cors"
+import fastifyJwt from "@fastify/jwt"
 import fastify from "fastify"
 
+import { authRoutes } from "./routes/auth"
 import { memoryRoutes } from "./routes/memories"
 
 const app = fastify()
@@ -8,8 +12,12 @@ const app = fastify()
 app.register(fastifyCors, {
   origin: "*",
 })
+app.register(fastifyJwt, {
+  secret: process.env.JWT_SECRET ?? "secret",
+})
 
 app.register(memoryRoutes, { prefix: "/memories" })
+app.register(authRoutes, { prefix: "/auth" })
 
 app
   .listen({ port: 3333 })

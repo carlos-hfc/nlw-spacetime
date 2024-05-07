086852b6c1debbe22c9394e9bae66063dbb03ad9
Carlos Faustino <chfcchfc96@gmail.com>
2024-05-07 09:14:49 -0300

    feat: update layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -5,6 +5,12 @@ import {
   Bai_Jamjuree as BaiJamjuree,
   Roboto_Flex as Roboto,
 } from "next/font/google"
+import { cookies } from "next/headers"
+
+import { Copyright } from "@/components/copyright"
+import { Hero } from "@/components/hero"
+import { Profile } from "@/components/profile"
+import { SignIn } from "@/components/sign-in"
 
 const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" })
 const baiJamjuree = BaiJamjuree({
@@ -24,12 +30,30 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const isAuthenticated = cookies().has("token")
+
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100 antialiased`}
       >
-        {children}
+        <main className="grid min-h-dvh grid-cols-2">
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+            {isAuthenticated ? <Profile /> : <SignIn />}
+
+            <Hero />
+
+            <Copyright />
+          </div>
+
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )

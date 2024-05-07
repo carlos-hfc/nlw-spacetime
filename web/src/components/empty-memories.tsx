daa2eef917ff8b338ec104490f15aee481a99f7f
Carlos Faustino <chfcchfc96@gmail.com>
2024-05-07 09:15:09 -0300

    feat: add new memories page

--- web/src/components/empty-memories.tsx
+++ web/src/components/empty-memories.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link"
+
 export function EmptyMemories() {
   return (
     <div className="flex flex-1 items-center justify-center">
       <p className="w-[360px] text-center leading-relaxed">
         Você ainda não registrou nenhuma lembrança, comece a{" "}
-        <a className="underline hover:text-gray-50">criar agora</a>!
+        <Link
+          href="/memories/new"
+          className="underline hover:text-gray-50"
+        >
+          criar agora
+        </Link>
+        !
       </p>
     </div>
   )
--- web/src/components/hero.tsx
+++ web/src/components/hero.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 import logoImg from "@/assets/logo.svg"
 
@@ -20,9 +21,12 @@ export function Hero() {
         </p>
       </div>
 
-      <a className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600">
+      <Link
+        href="/memories/new"
+        className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+      >
         CADASTRAR LEMBRANÇA
-      </a>
+      </Link>
     </div>
   )
 }

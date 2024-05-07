d18df373081be7afa80d0b8001feec015f4ab733
Carlos Faustino <chfcchfc96@gmail.com>
2024-05-07 09:54:58 -0300

    feat: add upload file route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,16 @@
 import "dotenv/config"
 
+import { resolve } from "node:path"
+
 import fastifyCors from "@fastify/cors"
 import fastifyJwt from "@fastify/jwt"
+import fastifyMultipart from "@fastify/multipart"
+import fastifyStatic from "@fastify/static"
 import fastify from "fastify"
 
 import { authRoutes } from "./routes/auth"
 import { memoryRoutes } from "./routes/memories"
+import { uploadRoutes } from "./routes/upload"
 
 const app = fastify()
 
@@ -15,9 +20,15 @@ app.register(fastifyCors, {
 app.register(fastifyJwt, {
   secret: process.env.JWT_SECRET ?? "secret",
 })
+app.register(fastifyMultipart)
+app.register(fastifyStatic, {
+  root: resolve(__dirname, "..", "uploads"),
+  prefix: "/uploads",
+})
 
 app.register(memoryRoutes, { prefix: "/memories" })
 app.register(authRoutes, { prefix: "/auth" })
+app.register(uploadRoutes, { prefix: "/upload" })
 
 app
   .listen({ port: 3333 })
